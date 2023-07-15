1b428833b4558a166f3823d399a9d3f5a855c108
sandorbence <szaniszlo47@gmail.com>
2023-07-15 19:07:16 +0200

    Add new page
    Added page for adding new wiki entries. Redirect works after creating
    the entry. Markdown to html conversion also works. Entries listed on
    index site are nwo clicable. Todo: further styling of the pages,
    implement random page, implemenet editing of pages.

--- project1/encyclopedia/urls.py
+++ project1/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("new", views.new_entry, name="newentry"),
+    path("wiki/<str:title>", views.entry, name="entry")
 ]
--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import forms
 
 from . import util
+
+from markdown2 import Markdown
+
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title of entry")
+    text = forms.CharField(label="", widget=forms.Textarea(
+        attrs={"placeholder": "Begin typing your wiki entry"}))
 
 
 def index(request):
@@ -8,3 +17,29 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def new_entry(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            util.save_entry(
+                form.cleaned_data["title"],
+                form.cleaned_data["text"])
+            return redirect('entry', title=form.cleaned_data["title"])
+    return render(request, "encyclopedia/new-entry.html", {
+        "form": NewEntryForm()
+    })
+
+
+def entry(request, title):
+    markdown_converter = Markdown()
+    try:
+        entry_converted = markdown_converter.convert(util.get_entry(title))
+    except:
+        return render(request, "404.html", {
+            "title": title.capitalize()
+        })
+    return render(request, "encyclopedia/entry.html", {
+        "title": title.capitalize(),
+        "entry": entry_converted
+    })
