8c622ae8d069acb07c11dd1489d39ee1694f049e
sandorbence <szaniszlo47@gmail.com>
2023-07-18 22:27:41 +0200

    Add styling
    Added further css styling and comments for the views. Maybe a little
    more styling and it's done.

--- project1/encyclopedia/urls.py
+++ project1/encyclopedia/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("search", views.search, name="search"),
     path("edit", views.edit_entry, name="edit"),
-    path("random", views.random_page, name="random")
+    path("random", views.random_page, name="random"),
+    path("save", views.save_entry, name="save"),
+    path("search", views.search, name="search")
 ]
--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -7,13 +7,14 @@ from markdown2 import Markdown
 
 from random import Random
 
+# Form for creating and editing entry contents
 class EntryForm(forms.Form):
     title = forms.CharField(label="Title of wiki page",  widget=forms.TextInput(attrs={"placeholder": "Title of entry"}))
     text = forms.CharField(label="", widget=forms.Textarea(
         attrs={"placeholder": "Begin typing your wiki entry"}))
     method = forms.CharField(widget=forms.HiddenInput(), required=False)
 
-
+# View for main route
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -26,6 +27,8 @@ def save_entry(request):
         if form.is_valid():
             title = form.cleaned_data["title"]
             text = form.cleaned_data["text"]
+            # If the function was called via editing,
+            # we can overwrite the existing entry
             if form.cleaned_data["method"] == "edit":
                 # Without encode() saving adds extra blank lines 
                 util.save_entry(title, text.encode())
@@ -36,13 +39,14 @@ def save_entry(request):
             else:
                 raise Exception("Wiki page with that title already exists.")
 
-
+# View for rendering an entry
 def entry(request, title):
     markdown_converter = Markdown()
     try:
         entry_converted = markdown_converter.convert(util.get_entry(title))
     except:
-        return render(request, "404.html", {
+        # Page not found exception page
+        return render(request, "encyclopedia/404.html", {
             "title": title.capitalize()
         })
     return render(request, "encyclopedia/entry.html", {
@@ -50,15 +54,17 @@ def entry(request, title):
         "entry": entry_converted
     })
 
-
+# View for handling search queries
 def search(request):
     if request.method == "POST":
         query = request.POST.get("q")
         entries = util.list_entries()
         matches = []
         for entry in entries:
+            # Go to entry page if there is a full match
             if query.lower() == entry.lower():
                 return redirect("entry", title=query)
+            # Add to the list of potential matches
             if query.lower() in entry.lower():
                 matches.append(entry)
         return render(request, "encyclopedia/search.html", {
@@ -66,20 +72,24 @@ def search(request):
             "matches": matches
         })
     
-
+# View for handling content editing/adding
 def edit_entry(request):
+    # If the function was called from an entry page,
+    # prefill with the already existing data
     if request.method == "POST":
         form = EntryForm({"title": request.POST.get("title"), "text": util.get_entry(request.POST.get("title")), "method": "edit"})
         return render(request, "encyclopedia/edit-entry.html", {
             "form": form,
             "pageTitle": "Edit Page"
         })
+    # If the function was called from the new page button,
+    # start with a blank form
     return render(request, "encyclopedia/edit-entry.html", {
         "form": EntryForm(),
         "pageTitle": "New Page"
     })
 
-
+# View for handling the random button
 def random_page(request):
     rand = Random()
     entries = util.list_entries()
