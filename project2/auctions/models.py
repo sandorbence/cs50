9254d90f48eb545e04758f9feb8b52802f34df5b
sandorbence <szaniszlo47@gmail.com>
2023-07-22 18:35:33 +0200

    Add models
    Added models for listings, bids and comments. Started implementing the
    index page and the listing details page.

--- project2/auctions/models.py
+++ project2/auctions/models.py
@@ -5,6 +5,27 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
 class Listing(models.Model):
-    title = models.CharField
-    pubDate = models.DateTimeField('date published')
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=120)
+    price = models.IntegerField()
+    pub_date = models.DateTimeField("date published")
+    photo = models.ImageField(upload_to="listing-images", blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Bid(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="bids")
+    date = models.DateTimeField("date of bid")
+    value = models.IntegerField()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments")
+    date = models.DateTimeField("date of comment")
+    text = models.CharField(max_length=120)
