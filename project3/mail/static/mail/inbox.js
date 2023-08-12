4ac4893189832a2f6986019a010e7f85593ee06c
sandorbence <szaniszlo47@gmail.com>
2023-08-12 13:59:53 +0200

    Implement email view
    Added single email view. User now can see their inbox, sent and archived
    mailboxes and able to click in single emails and watch their content.

--- project3/mail/static/mail/inbox.js
+++ project3/mail/static/mail/inbox.js
@@ -17,6 +17,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#single_email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -30,28 +31,51 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single_email-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  switch (mailbox) {
-    case 'inbox':
-      break;
-    case 'sent':
-      break;
-    case 'archive':
-      break;
-    default:
-      break;
-  }
+  // Get emails in mailbox
+  fetch('/emails/' + mailbox)
+    .then(response => response.json())
+    .then(emails => {
+      console.log(emails);
+
+      emails.forEach(email => {
+        console.log(email.body);
+        create_mail_element(email);
+      });
+    });
+}
+
+function load_email(email) {
+
+  // Hide mailbox and compose
+  document.querySelector("#emails-view").style.display = 'none';
+  document.querySelector("#compose-view").style.display = 'none';
+  document.querySelector('#single_email-view').style.display = 'block';
+
+  const container = document.querySelector('#single_email-view');
+
+  // Clear div (delete last loaded email)
+  container.innerHTML = '';
+
+  // Create email element
+  container.append(create_element('sender', email.sender));
+  container.append(create_element('subject', email.subject));
+  container.append(create_element('body', email.body));
+  container.append(create_element('timestamp', email.timestamp));
 }
 
 function send_email() {
 
-  let recipients = document.querySelector('#compose-recipients').value
-  let subject = document.querySelector('#compose-subject').value
-  let body = document.querySelector('#compose-body').value
+  // Get data from form
+  let recipients = document.querySelector('#compose-recipients').value;
+  let subject = document.querySelector('#compose-subject').value;
+  let body = document.querySelector('#compose-body').value;
 
+  // Add email to database
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
@@ -62,8 +86,48 @@ function send_email() {
   })
     .then(response => response.json())
     .then(result => {
-      console.log(result)
-    })
+      console.log(result);
+      load_mailbox('sent');
+    });
 
   return false;
+}
+
+function create_mail_element(email) {
+  let element = document.createElement('div');
+  element.classList.add('email');
+  if (email.read) element.classList.add('read');
+
+  // Create each part of email element
+  element.append(create_element('sender', email.sender));
+  element.append(create_element('subject', email.subject));
+  element.append(create_element('timestamp', email.timestamp));
+
+  // Add event listener to email element
+  element.addEventListener('click', () => {
+    get_email(email.id);
+  });
+
+  // Add element to main container
+  document.querySelector('#emails-view').append(element);
+}
+
+function create_element(type, text) {
+
+  // Create separate div for each part of email
+  let element = document.createElement('div');
+  element.innerHTML = text;
+  element.classList.add(type);
+
+  return element;
+}
+
+function get_email(id) {
+  fetch('/emails/' + id)
+    .then(response => response.json())
+    .then(email => {
+      console.log(email);
+
+      load_email(email);
+    });
 }
