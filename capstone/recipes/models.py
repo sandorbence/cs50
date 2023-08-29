7714ebab0251b72e0916e59635fa4299c3236ded
sandorbence <szaniszlo47@gmail.com>
2023-08-29 22:03:33 +0200

    Iinitial commit

---
+++ capstone/recipes/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Recipe(models.Model):
+    uploader = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipes")
+    image = models.ImageField(upload_to="recipe-images", blank=True, null=True)
+    preparation = models.CharField(max_length=2500)
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="ingredients")
+    name = models.CharField(max_length=40)
+    quantity = models.PositiveIntegerField()
+    unit = models.CharField(max_length=10, choices=[
+        ("ml", "ml"),
+        ("l", "l"),
+        ("g", "g"),
+        ("kg", "kg"),
+        ("tsp", "tsp")
+    ])
---
+++ capstone/recipes/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+
+# Code copied from project2
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            if request.POST.get("next"):
+                return redirect(request.POST.get("next"))
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        # Redirect user to previously requested page
+        if request.GET.get("next"):
+            return render(request, "auctions/login.html", {
+                "next": request.GET.get("next")
+            })
+        return render(request, "auctions/login.html")
+
+# Code copied from project2
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+# Code copied from project2
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/register.html")
